1d0c62ee6464277c1f0d3902c6d473e5984747c7
Lê Chí Tuyền <lctuyenvn@gmail.com>
2022-07-16 05:22:48 +0700

    Cập nhật formGraph để gửi dữ liệu lên mysql

--- ThongTinTienAo/formGraph.cs
+++ ThongTinTienAo/formGraph.cs
@@ -12,6 +12,7 @@ using CoinGecko.Clients;
 using CoinGecko.Entities.Response.Coins;
 using CoinGecko.Interfaces;
 using CoinGecko.Parameters;
+using MySql.Data.MySqlClient;
 
 namespace ThongTinTienAo
 {
@@ -42,16 +43,72 @@ namespace ThongTinTienAo
         }
         private async void fillchart()
         {
-            var result = await _client.CoinsClient.GetMarketChartsByCoinId(_nameCoin, "usd", "max");
-            foreach(var _ in result.Prices)
+            
+            try
             {
-                _[0] = _[0] / 1000;
-                Console.Write(FromUnixTime((long)_[0]));
-                Console.Write(", ");
-                Console.WriteLine(_[1]);
+                string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=geckocoin_data;";
+                string query = " CREATE TABLE `geckocoin_data`.`" + _nameCoin + "` (`time` INT NOT NULL , `price` DOUBLE NOT NULL ) ENGINE = InnoDB;";
 
-                chtCoin.Series[0].Points.AddXY(FromUnixTime((long)_[0]).ToString().Substring(0,11),_[1]);
-            }    
+                MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+                MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+                commandDatabase.CommandTimeout = 60;
+
+                databaseConnection.Open();
+                MySqlDataReader myReader = commandDatabase.ExecuteReader();
+
+                MessageBox.Show("Tạo bảng oke");
+
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=geckocoin_data;";
+                string query = "TRUNCATE TABLE " + _nameCoin + ";";
+
+                MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+                MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+                commandDatabase.CommandTimeout = 60;
+                // Show any error message.
+                databaseConnection.Open();
+
+                MessageBox.Show(ex.Message);
+            }
+            StringBuilder command = new StringBuilder();
+
+            var result = await _client.CoinsClient.GetMarketChartsByCoinId(_nameCoin, "usd", "max");
+            foreach(var value in result.Prices)
+            {
+                value[0] = value[0] / 1000;
+                Console.Write(FromUnixTime((long)value[0]));
+                Console.Write(", ");
+                Console.WriteLine(value[1]);
+
+                chtCoin.Series[0].Points.AddXY(FromUnixTime((long)value[0]).ToString().Substring(0,11), value[1]);
+                //query = "INSERT INTO " + _nameCoin + "(`time`, `price`) VALUES ('" + value[0] + "', '" + value[1] + "');";
+                command.Append("INSERT INTO " + _nameCoin + "(`time`, `price`) VALUES ('" + value[0] + "', '" + value[1] + "');");
+
+
+            }
+            try
+            {
+                string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=geckocoin_data;";
+
+                MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+                MySqlCommand commandDatabase = new MySqlCommand(command.ToString(), databaseConnection);
+                commandDatabase.CommandTimeout = 60;
+
+                databaseConnection.Open();
+                MySqlDataReader myReader = commandDatabase.ExecuteReader();
+
+                MessageBox.Show("Cập nhật dữ liệu Oke");
+
+                databaseConnection.Close();
+            }
+            catch
+            {
+
+            }
+
         }
     }
 }
